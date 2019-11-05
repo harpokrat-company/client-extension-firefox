31aea09559a6037e7093bf2a03a2a2c84baa7653
Antoine Drouot <antoine@antoine.drouot>
2019-11-05 11:43:22 +0000

    Correction du router + ajout d'un affichage du token pour présentation EIP

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -18,15 +18,22 @@ export class LoginComponent implements OnInit {
 
   email: String = "";
   password: String = "";
+  failure: boolean = false;
+  token: String = "";
+
   connexion(): void {
     if (this.email.length > 0 && this.password.length > 0) {
       this.tokenService.login(this.email.toString(), this.password.toString()).subscribe(
         success => {
           console.log('success');
           console.log(success);
-          this.router.navigateByUrl('/home').then()
+          this.token = success.attributes.token;
+          this.failure = false;
+          //this.router.navigate(['/homepage']);
         },
         error => {
+          this.failure = true;
+          this.token = "";
           console.log(error)
         }
       );  
